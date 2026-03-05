21d93cdead4d9bb98d61be9310948ee78de778d7
khola azeem <kholaazeem104@gmail.com>
2026-03-05 23:06:54 +0500

    navbar done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import React from 'react'
+import { FaPhoneAlt,FaBars ,FaTimes } from "react-icons/fa";
+import Button  from './Button';
+
+const Navbar = () => {
+
+  const [isMenuOpen , setIsMenuOpen] = useState(false);
+  return (
+    <nav className="fixed-top p-3 w-100 d-flex justify-content-center position-relative">
+        <div className="glass-nav d-flex justify-content-between align-items-center px-4 py-2">
+            {/* Logo section */}
+            <div className='fw-bold fs-5' style={{color: '#1B4D3E'}}>
+              Great Chalkboard Co.
+            </div>
+            {/* Links section */}
+            <div className='d-none d-md-flex align-items-center gap-4'>
+              <a href="#" className="nav-link active-link">Home</a>
+              <a href="#" className="nav-link ">About</a>
+              <a href="#" className="nav-link ">Shop</a>
+              <a href="#" className="nav-link ">Terms</a>
+            </div>
+            {/* Call info */}
+            <div className='d-none d-md-block'>
+            <Button
+              text = "Call for Info"
+              variant = "primary"
+              icon = {<FaPhoneAlt/>}
+              />
+            </div>
+
+            <div className='d-md-none fs-4' style={{cursor: 'pointer' , color: '#1B4D3E'}} onclick ={()=>setIsMenuOpen(!isMenuOpen)}>
+              isMenuOpen ? <FaTimes/> : <FaBars />
+            </div>
+
+            {isMenuOpen && (
+              <div 
+            className="d-md-none position-absolute w-90 glass-nav d-flex flex-column align-items-center py-4 gap-3" 
+            style={{ top: '80px', left: '5%', right: '5%' }}
+          >
+              <a href="#" className="nav-link active-link">Home</a>
+              <a href="#" className="nav-link">About</a>
+              <a href="#" className="nav-link">Shop</a>
+              <a href="#" className="nav-link">Terms</a>
+              <Button text="Call for Info" variant="primary" icon={<FaPhoneAlt/>} />
+          </div>
+            )}
+            
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
